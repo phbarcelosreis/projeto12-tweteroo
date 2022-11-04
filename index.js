3969ab60e1ff3eeedb84e011b2b0b793a5719d0a
Pedro Reis <phbarcelosreis66@gmail.com>
2022-11-03 21:29:28 -0300

    att

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-///testes
+import express from "express";
+import cors from "cors"
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const login = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = req.body;
+
+    if(!username || !avatar){
+        return res.status(400);
+    }
+
+    const user = {
+        username,
+        avatar
+    }
+
+    login.push(user)
+    res.send(user)
+})
+
+app.get("/sign-up", (req, res) => {
+
+    res.send(login)
+
+})
+
+app.listen(5000, () => console.log("Server running at port: 5000"))
